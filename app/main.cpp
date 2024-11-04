cdf989e614ac25876ed8db930f7efb218c20b1bf
Tim Krebs <timkrebs9@gmail.com>
2024-11-04 13:03:55 +0100

    Add ImGui and GLFW includes for rendering support

--- app/main.cpp
+++ app/main.cpp
@@ -10,6 +10,23 @@
 #include "config.hpp"
 #include "foo.h"
 
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include "imgui_impl_opengl3_loader.h"
+
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
+#include <GLFW/glfw3.h>
+
+#include "render.hpp"
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) &&                                 \
+    !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
+
 using json = nlohmann::json;
 namespace fs = std::filesystem;
 
